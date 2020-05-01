fccbdb4b6a82d3e71671efd23f8d0937164a987e
Radek Palisa <radek.palisa@gmail.com>
2020-05-01 23:42:25 +0200

    project 4: I/O handling

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,72 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+    @SCREEN
+    D=A
+    @arr
+    M=D
+    @8192 
+    D=A
+    @n // -> screen length = 8192
+    M=D
+    @color
+    M=0
+    @current_index
+    M=0
+(LOOP)
+    @KBD
+    D=M
+    @CLEAR_COLOR
+    D;JEQ // if KBD = 0
+
+    @SET_COLOR
+    0;JMP // Goto LOOP
+(SET_COLOR)
+    @color
+    D=M
+    @LOOP
+    D;JLT // if current color already black
+
+    @color
+    M=-1
+    @PAINT
+    0;JMP // Goto PAINT
+(CLEAR_COLOR)
+    @color
+    D=M
+    @LOOP
+    D;JEQ // if current color already white
+
+    @color
+    M=0
+    @PAINT
+    0;JMP // Goto PAINT
+(PAINT)
+    @i
+    M=0 // reset i
+(PAINT_LOOP)
+    @i
+    D=M
+    @n
+    D=D-M
+    @LOOP // if i==n, exit
+    D;JEQ
+
+    // current index = color
+    @arr
+    D=M
+    @i
+    D=D+M
+    @current_index
+    M=D
+    @color 
+    D=M 
+    @current_index
+    A=M
+    M=D
+
+    // i++
+    @i
+    M=M+1
+    @PAINT_LOOP
+    0;JMP
